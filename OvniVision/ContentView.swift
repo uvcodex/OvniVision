aafd62b393440c9524e5206ef78ddd6aa757955d
medusza <uvazquez085@gmail.com>
2026-03-09 09:27:12 -0700

    First commit

---
+++ OvniVision/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OvniVision
+//
+//  Created by Ulises Vazquez on 3/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OvniVision/OvniVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  OvniVisionApp.swift
+//  OvniVision
+//
+//  Created by Ulises Vazquez on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct OvniVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
